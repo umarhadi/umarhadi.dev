a78240eb68ad3d31d815e960a2e3ece976696eb5
umarhadi <hi@umarhadi.dev>
2023-06-13 10:56:34 +0700

    update
    Signed-off-by: umarhadi <hi@umarhadi.dev>

---
+++ _next/static/0z4F9_G0RnCNpYTfLz9r8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/4267-8e395633e1707634.js","static/chunks/pages/index-508bb8e8286b9964.js"],"/_error":["static/chunks/pages/_error-f9f90c4a31e5f823.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
